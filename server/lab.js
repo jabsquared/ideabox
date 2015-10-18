d3a37ee4017a53f48923b10393df1fd125637513
Louis Vichy <lgvichy@gmail.com>
2015-10-18 02:06:11 -0700

    Testing API, simple gentle

--- server/lab.js
+++ server/lab.js
@@ -1,31 +1,37 @@
 var socketio = require('socket.io');
 var restify = require('restify');
+var request = require('request');
+
 var server = restify.createServer();
 var io = socketio.listen(server.server);
-var helena = require('./js/helena');
-var sideria = require('./js/sideria');
+
+var hekate = require('./modulas/hekate');
+var minerva = require('./modulas/minerva');
+
+server.use(restify.bodyParser());
 
 server.listen(process.env.VCAP_APP_PORT || 1314, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
 
+function postConcept(req, res, next) {
+  // body...
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+
+  var concept = req.params.c;
+
+  // Return back a package of related concept with weight
+  // request(options, function(error, response, body) {
+  //   res.send(200, body);
+  // });
+
+  res.send(200, concept);
+}
+
+server.get('/p/:c', postConcept);
+
 io.sockets.on('connection', function(socket) {
   // Define a helper function to return the count.
-  var emitCount = function(err, count) {
-    if (err || !count) {
-      return console.log(err);
-    }
-    if (count) {
-      io.sockets.emit('Population Changed', count);
-    }
-  };
-  socket.on('Image Clicked', function(data) {
-    // console.log(data);
-    if (!data) {
-      sideria.count(emitCount);
-      return;
-    }
-    sideria.countDown(emitCount);
-    helena.receive(data);
-  });
+
 });
--- server/modulas/minerva.js
+++ server/modulas/minerva.js
@@ -6,12 +6,26 @@
 "use strict";
 
 var secret = require('./hekate')
-  .auth.qna;
+  .auth.cti;
 
-exports.options = function(question) {
+exports.accounts = function () {
   return {
-    url: secret.url + '/v1/question/healthcare/',
-    method: 'POST',
+    url: secret.url + '/v2/accounts',
+    method: 'GET',
+    headers: {
+      'X-synctimeout': '30'
+    },
+    auth: {
+      'user': secret.usn,
+      'password': secret.pwd
+    },
+  };
+};
+
+exports.graphs = function(question) {
+  return {
+    url: secret.url + '/v2/graphs/wikipedia/en-20120601/',
+    method: 'GET',
     headers: {
       'X-synctimeout': '30'
     },
