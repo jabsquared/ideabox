00098e05540315ddaad03501c635850d4040e93e
Yonas Berhe <sanoy@D-108-179-131-186.dhcp4.washington.edu>
2015-10-18 11:32:50 -0700

    Event on enter for form

--- client/js/call.js
+++ client/js/call.js
@@ -1,46 +1,79 @@
 String.prototype.capitalize = function() {
-  return this.toLowerCase()
-    .replace(/\b\w/g, function(m) {
-      return m.toUpperCase();
-    });
+    return this.toLowerCase()
+        .replace(/\b\w/g, function(m) {
+            return m.toUpperCase();
+        });
 };
+
 function recapData(data) {
-  // body...
-  for (var i = 0; i < data.concepts.length; i++) {
-    data.concepts[i].concept = data.concepts[i].concept.label;
-    data.concepts[i].group = data.concepts[0].concept;
-  }
-  return data;
+    // body...
+    for (var i = 0; i < data.concepts.length; i++) {
+        data.concepts[i].concept = data.concepts[i].concept.label;
+        data.concepts[i].group = data.concepts[0].concept;
+    }
+    return data;
 }
 var userData;
 
 $(document)
-  .ready(function() {
-    var word;
-    $('#submit-btn')
-      .click(function() {
+    .ready(function() {
+        var word;
+        $('#submit-btn')
+            .click(function() {
+                // console.log("Click!");
+                word = $('#word')
+                    .val();
+
+                $.get("https://ideabox.mybluemix.net/p/" +
+                    word.capitalize(),
+                    function(data) {
+                        // data = JSON.parse(data);
+                        // console.log(JSON.parse(data));
+                        var js = JSON.parse(data);
+                        console.log(js);
+                        if (!js.error) {
+                            js = recapData(js);
+                            if (!userData) {
+                                userData = js;
+                            } else {
+                                userData.concepts = userData.concepts.concat(js.concepts);
+                            }
+                            render(userData);
+                        } else {
+                            alert("NOT FOUND!");
+                        }
+                    });
+            });
+    });
+
+
+$(document.body).delegate('input:text', 'keypress', function(e) {
+    if (e.which === 13) { // if is enter
+        e.preventDefault(); // don't submit form
+        var word;
+        // do what you want here
         // console.log("Click!");
         word = $('#word')
-          .val();
+            .val();
 
         $.get("https://ideabox.mybluemix.net/p/" +
-          word.capitalize(),
-          function(data) {
-            // data = JSON.parse(data);
-            // console.log(JSON.parse(data));
-            var js = JSON.parse(data);
-            console.log(js);
-            if (!js.error) {
-              js = recapData(js);
-              if (!userData) {
-                userData = js;
-              } else {
-                userData.concepts = userData.concepts.concat(js.concepts);
-              }
-              render(userData);
-            } else {
-              alert("NOT FOUND!");
-            }
-          });
-      });
-  });
+            word.capitalize(),
+            function(data) {
+                // data = JSON.parse(data);
+                // console.log(JSON.parse(data));
+                var js = JSON.parse(data);
+                console.log(js);
+                if (!js.error) {
+                    js = recapData(js);
+                    if (!userData) {
+                        userData = js;
+                    } else {
+                        userData.concepts = userData.concepts.concat(js.concepts);
+                    }
+                    render(userData);
+                } else {
+                    alert("NOT FOUND!");
+                }
+            });
+    }
+});
